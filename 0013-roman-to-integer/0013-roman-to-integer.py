47a99e037a74f54d7c7096836b1b7c82d26b94f6
Kurt Bembridge <kurt_bembridge@yahoo.com>
2024-06-12 20:51:38 -0400

    Time: 39 ms (89.23%), Space: 16.4 MB (99.17%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def romanToInt(self, s: str) -> int:
         encounter = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
@@ -14,3 +15,4 @@ class Solution:
             last_number = encounter[i]
 
         return num_total
+        
